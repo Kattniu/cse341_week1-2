d2a72a8b0e83fa8288d94a4145a7a7538ded28f5
Kattniu <kathe.adriana24@gmail.com>
2025-09-14 22:44:57 +0200

    week02

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 
-router.use('/api-docs', swaggerUi.server);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
